78e2aed66fb7d50bc27664daf76d0f3298e64c4d
Andrey Galchevski <andreygalchevski@gmail.com>
2019-01-13 14:26:21 +0200

    change api url

--- src/store/Api.js
+++ src/store/Api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 let baseURL = "http://localhost:3000";
 
 if (process.env.NODE_ENV === "production") {
-  baseURL = `http://www.insapient.org`;
+  baseURL = `https://insapient-api.herokuapp.com`;
 }
 
 export default () => {
